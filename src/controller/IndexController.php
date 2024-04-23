420076c08c3ea27c4621a26adba1a2cab52ce2c4
ZAKRZU <mznicko655@gmail.com>
2024-04-23 20:56:28 +0200

    Added whois information

--- src/controller/IndexController.php
+++ src/controller/IndexController.php
@@ -9,23 +9,31 @@ use Zakrzu\DDC\Entity\DomainEntity;
 use Zakrzu\DDC\Manager\DomainChecker;
 use Zakrzu\DDC\Manager\RedirectManager;
 
+use Iodev\Whois\Factory;
+use Iodev\Whois\Whois;
+
 class IndexController
 {
     private $version = App::VERSION;
     private $db = null;
     private ?DomainInfo $mainDomain = null;
     private string $txtLookup = "";
+    private ?Whois $whois = null;
+    private bool $domainIsAvailable = true;
 
     public function __construct()
     {
         $this->db = App::$app->getDb();
+        $this->whois = Factory::get()->createWhois();
 
         if (isset($_GET["txt"])) {
             $this->txtLookup = $_GET["txt"];
         }
 
         if (isset($_GET["lookup"])) {
-            $this->mainDomain = new DomainInfo($this->parseDomain($_GET["lookup"]));
+            $parsed = $this->parseDomain($_GET["lookup"]);
+            $this->mainDomain = new DomainInfo($parsed);
+            $this->domainIsAvailable = $this->whois->isDomainAvailable($parsed);
             $this->index();
         } else {
             $this->form();
@@ -38,6 +46,10 @@ class IndexController
         * This variables are provided for template rendering
         */
         if ($this->mainDomain->dnsZoneExist()) {
+            if (!$this->domainIsAvailable) {
+                $domainWhois = $this->whois->loadDomainInfo($this->mainDomain->getDomainName());
+                $domainWhoisRaw = $this->whois->lookupDomain($this->mainDomain->getDomainName())->text;
+            }
             if ($this->db) {
                 $manager = new DomainChecker();
                 $dEntity = new DomainEntity($this->mainDomain->getDomainName(), 'now');
