6dda86718b99d92d6389b023685c062f6befd407
ZAKRZU <mznicko655@gmail.com>
2024-05-31 04:11:25 +0200

    Improved redirect template
    * Added Popper js library
    * Domain addresses longer than 45 characters will be truncated
    * Fixed DNS template bug

--- public/js/app.js
+++ public/js/app.js
@@ -70,4 +70,6 @@ function loadTheme() {
 
 (function() {
     loadTheme();
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 })();
